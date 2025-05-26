3bcff84a9588fe6efd7a2e62be0cd61d3108209e
OlsenFischer <127721354+olsen-fischer@users.noreply.github.com>
2025-05-26 11:16:12 +0300

    rf

--- obj/Debug/net8.0/RegisterAnimals.AssemblyInfo.cs
+++ obj/Debug/net8.0/RegisterAnimals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegisterAnimals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e892dc8ede064c2e4850995b9bba910bd77ee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75b33a88814734ffdda377fc5be10dc874fc912")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegisterAnimals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegisterAnimals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
